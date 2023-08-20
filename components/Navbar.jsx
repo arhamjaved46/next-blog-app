25e452b481b7cf572e6fb8ab9019410dc0373503
Arham Javed <arhamjaved46@gmail.com>
2023-08-21 03:27:08 +0500

    again change styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed flex items-center justify-between w-full p-8 border bg-slate-50">
+    <nav className="fixed flex items-center justify-between w-full p-8 border bg-slate-100">
       <Link href={"/"}>
         <Image src={"../next.svg"} alt="Next-logo" width={120} height={100} />
       </Link>
