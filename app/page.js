bf5a80a8405e40075a81810062a14efbb2f4c52e
Arham Javed <arhamjaved46@gmail.com>
2023-08-21 01:38:43 +0500

    remove feed file

--- app/page.js
+++ app/page.js
@@ -1,10 +1,36 @@
-import Feed from "@/components/Feed";
+import SinglePost from "@/components/SinglePost";
+
+const fetchPosts = async () => {
+  const URI = process.env.NEXT_PUBLIC_API_URL;
+  const response = await fetch(`${URI}/api/posts`, {
+    cache: "no-store"
+  });
+  if (!response.ok) {
+    console.log("Fetching posts failed");
+  }
+  return response.json();
+
+};
 
 export default async function Home() {
+
+  const { posts } = await fetchPosts();
+
   return (
     <div className="my-28">
       <h1 className="my-4 text-2xl font-semibold">Latest Blog Posts</h1>
-      <Feed />
+
+      {posts ? (
+
+        posts.map((p) => (
+          <SinglePost p={p} key={p._id} />
+        ))
+
+      ) : (
+        "Loading..."
+      )
+      }
+
       <h3 className="font-bold text-center text-gray-600">***Display only first 10 posts for now 🙂***</h3>
     </div>
   );
