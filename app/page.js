13628b10d14b27d351a7168b07cd049d4a29a5f9
Arham Javed <arhamjaved46@gmail.com>
2023-08-21 01:35:41 +0500

    Add new file Feed into homepage

--- app/page.js
+++ app/page.js
@@ -1,36 +1,10 @@
-import SinglePost from "@/components/SinglePost";
-
-const fetchPosts = async () => {
-  const URI = process.env.NEXT_PUBLIC_API_URL;
-  const response = await fetch(`${URI}/api/posts`, {
-    cache: "no-store"
-  });
-  if (!response.ok) {
-    console.log("Fetching posts failed");
-  }
-  return response.json();
-
-};
+import Feed from "@/components/Feed";
 
 export default async function Home() {
-
-  const { posts } = await fetchPosts();
-
   return (
     <div className="my-28">
       <h1 className="my-4 text-2xl font-semibold">Latest Blog Posts</h1>
-
-      {posts ? (
-
-        posts.map((p) => (
-          <SinglePost p={p} key={p._id} />
-        ))
-
-      ) : (
-        "Loading..."
-      )
-      }
-
+      <Feed />
       <h3 className="font-bold text-center text-gray-600">***Display only first 10 posts for now 🙂***</h3>
     </div>
   );
