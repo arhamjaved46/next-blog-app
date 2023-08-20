979ed37a2e34ea1bdcd39baefffe9dafabc9600c
Arham Javed <arhamjaved46@gmail.com>
2023-08-21 03:30:11 +0500

    again change styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 flex items-center justify-center w-full p-4 bg-white border">
-            <h3 className="text-center">Made with ❤️ by <a target="_blank" className="font-bold text-red-600" href="https://www.google.com/search?q=arhamjaved46">Arham Javed! | <a target="_blank" href="https://www.bing.com/search?q=arhamjaved46" className="font-bold text-red-600 text">arhamjaved46</a></a>
-            </h3>
+            <h3 className="text-center">Made with ❤️ by <a target="_blank" className="font-bold text-red-600" href="https://www.google.com/search?q=arhamjaved46">Arham Javed</a> | <a target="_blank" href="https://www.bing.com/search?q=arhamjaved46" className="font-bold text-red-600 text">@arhamjaved46</a></h3>
         </footer>
     )
 };
