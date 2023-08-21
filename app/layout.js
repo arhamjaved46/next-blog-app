5233b2769f815f2de1096e891b74f07eaeebb04c
Arham Javed <arhamjaved46@gmail.com>
2023-08-21 20:52:29 +0500

    add web analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           <Navbar />
           <div className="max-w-[1024px] mx-auto p-8">
           {children}
+          <Analytics />
           </div>
           <Footer />
         </main>
